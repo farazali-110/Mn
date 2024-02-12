ba3f2a1a6adf4e7f692386608023efd96f5b3322
farazali-110 <151817304+farazali-110@users.noreply.github.com>
2024-02-12 20:07:48 +0500

    Add files via upload

---
+++ logical2.py
@@ -0,0 +1,9 @@
+number=int(input("enter age:"))
+if number <=5:
+    print("you are child:")
+elif number<=10:
+    print("you are teenager:")
+elif number<20:
+     print("you are adult:")
+else:
+    print("you are senior citizen:")
